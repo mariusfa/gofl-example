641452fd3eed095810fb446c5b9a8ec71ac554a9
Marius Fagerland <mariusfagerland@yahoo.no>
2024-02-26 21:58:13 +0100

    Setup controllers and services for todo

--- todo/app/controllers.go
+++ todo/app/controllers.go
@@ -2,20 +2,24 @@ package app
 
 import (
 	"net/http"
+	"todo/internal/domain"
 
 	hc "github.com/mariusfa/gofl/health-controller"
 )
 
 type Controllers struct {
 	health *hc.HealthController
+	todo  *TodoController
 }
 
-func NewControllers() *Controllers {
+func NewControllers(services *domain.Services) *Controllers {
 	return &Controllers{
 		health: hc.NewHealthController(),
+		todo: NewTodoController(services.Todo),
 	}
 }
 
 func (c *Controllers) RegisterRoutes(router *http.ServeMux) {
 	c.health.RegisterRoutes(router)
+	c.todo.RegisterRoutes(router)
 }
--- todo/cmd/main.go
+++ todo/cmd/main.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 	"net/http"
 	"todo/app"
+	"todo/internal/domain"
 
 	"github.com/mariusfa/gofl/config"
 )
 
 func setup() *http.ServeMux {
 	router := http.NewServeMux()
-	controllers := app.NewControllers()
+	services := domain.NewServices()
+	controllers := app.NewControllers(services)
 	controllers.RegisterRoutes(router)
 
 	return router
