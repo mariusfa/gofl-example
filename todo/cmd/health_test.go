e935b3789b411b08392d4b6e0318ace2cbd4e1a4
Marius Fagerland <marius.fagerland@sparebank1.no>
2024-03-26 13:59:18 +0100

    fix tests

--- todo/cmd/health_test.go
+++ todo/cmd/health_test.go
@@ -4,10 +4,24 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"todo/internal/services"
+	"todo/internal/services/todo"
 )
 
+type todoRepoFake struct{}
+
+func (t *todoRepoFake) GetTodos() ([]todo.Todo, error) {
+	return []todo.Todo{}, nil
+}
+
 func TestHealth(t *testing.T) {
-	router := setup()
+	todoFake := &todoRepoFake{}
+	todoService := todo.NewTodoService(todoFake)
+	services := services.Services{
+		Todo: todoService,
+	}
+
+	router := setup(&services)
 	req, err := http.NewRequest("GET", "/health", nil)
 	if err != nil {
 		t.Fatal(err)
