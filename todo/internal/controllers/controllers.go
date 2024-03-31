e81a361ecf81b6a1dbcd89b3d9bccaa8aa3bee1c
Marius Fagerland <marius.fagerland@sparebank1.no>
2024-03-31 15:41:35 +0200

    Add metrics controller

--- todo/internal/controllers/controllers.go
+++ todo/internal/controllers/controllers.go
@@ -6,21 +6,25 @@ import (
 	"todo/internal/services"
 
 	hc "github.com/mariusfa/gofl/v2/health-controller"
+	mc "github.com/mariusfa/gofl/v2/metrics-controller"
 )
 
 type Controllers struct {
-	health *hc.HealthController
-	todo   *todo.TodoController
+	health  *hc.HealthController
+	metrics *mc.MetricsController
+	todo    *todo.TodoController
 }
 
 func New(services *services.Services) *Controllers {
 	return &Controllers{
-		health: hc.NewHealthController(),
-		todo:   todo.NewTodoController(services.Todo),
+		health:  hc.NewHealthController(),
+		metrics: mc.NewMetricsController(),
+		todo:    todo.NewTodoController(services.Todo),
 	}
 }
 
 func (c *Controllers) RegisterRoutes(router *http.ServeMux) {
 	c.health.RegisterRoutes(router)
+	c.metrics.RegisterRoutes(router)
 	c.todo.RegisterRoutes(router)
 }
